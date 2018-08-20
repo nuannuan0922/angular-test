22a09eea0880bd108b2f6db2202eb80e45479702
amy.huo <amy.huo@ucloud.cn>
2018-08-20 14:36:39 +0800

    stash

--- runoob-test/src/app/hero.service.ts
+++ runoob-test/src/app/hero.service.ts
@@ -4,20 +4,37 @@ import { Heroes} from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { MessageService } from './message.service';
 import { of } from 'rxjs/observable/of';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: Http,private messageService: MessageService) { }
+
+  private herosUrl = 'api/heroes';
 
   getHeroes() : Promise<Hero[]> {
-    return Promise.resolve(Heroes);
+    // return Promise.resolve(Heroes);
+    return this.http.get(this.herosUrl)
+                    .toPromise()
+                    .then(response => response.json().data as Hero[])
+                    .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('an error occurred: ', error);
+    return Promise.reject(error.message || error);
   }
 
   getHero(id: Number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(Heroes.find(hero => hero.id === id))
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
